3472e540a7b795c5f921260c24d0d6d0c6df5b1a
Kluvert's Macbook <angel777@Kluverts-MacBook.local>
2023-03-06 11:31:00 +0100

    flex div

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import img1 from "./images/Europecar.png"
-import img2 from "./images/calendar.png"
-import img3 from "./images/search.png"
-import img4 from "./images/wallet.png"
+import europecar from "./images/Europecar.png"
+import flexible from "./images/calendar.png"
+import search from "./images/search.png"
+import wallet from "./images/wallet.png"
+import covid from "./images/covid.png"
+import email_signup from "./images/email_signup.png"
 
 
 class Home extends Component {
@@ -10,7 +12,7 @@ class Home extends Component {
         return (
             <div>
 
-                <div className="container, display-flex, header">
+                <div>
 
                     <h3>Rentalcars.com Brand Logo</h3>
                     <button >Manage booking </button>
@@ -26,42 +28,73 @@ class Home extends Component {
                     <h4>Connecting you to the biggest brands in car rental
                     </h4>
 
-                    <img src={img1} alt="/" />
+                    <img src={europecar} alt="/" />
                 </div>
                 <br></br>
                 <br></br>
                 <br></br>
 
 
-                <div className="flex">
+                <div className="flexi">
 
-                    <div>
-                        <img src={img2} alt="/" />
-                        <h3>Flexible rentals
-                        </h3>
-                        <p>Cancel or change most bookings for free up to 48 hours before pick-up
-                        </p>
+                    <div className="flex" >
+                        <img src={flexible} alt="/" />
+
+                        <div>
+                            <h3>Flexible rentals
+                            </h3>
+                            <p>Cancel or change most bookings for
+                                <br />
+                                free up to 48 hours before pick-up
+                            </p>
+                        </div>
+
                     </div>
 
-                    <div>
-                        <img src={img3} alt="/" />
-                        <h3>No hidden fees
+                    <div className="flex">
+                        <img src={search} alt="/" />
+                        <div><h3>No hidden fees
                         </h3>
-                        <p>Know exactly what you’re paying
-                        </p>
+                            <p>Know exactly what you’re paying
+                            </p></div>
+
                     </div>
 
-                    <div>
-                        <img src={img3} alt="/" />
-                        <h3>Price Match Guarantee
-                        </h3>
-                        <p>Found the same deal for less? We’ll match the price.
-                        </p>
+                    <div className="flex"> 
+                        <img src={wallet} alt="/" />
+                        <div>
+                            <h3>Price Match Guarantee
+                            </h3>
+                            <p>Found the same deal for less? We’ll match the price.
+                            </p>
+                        </div>
+
                     </div>
 
 
                 </div>
 
+                <div>
+
+                    <div>
+                        <img src={covid} alt="/" />
+                        <h3>Clean cars. Flexible bookings. Socially distant rental counters.
+                        </h3>
+                        <p>We’re working with our partners to keep you safe and in the driving seat.
+                        </p>
+                        <p>Find out more</p>
+                    </div>
+                    <div>
+                        <img src={email_signup} alt="/" />
+                        <h3>Subscribe for exclusive offers and deals!
+                        </h3>
+                        <form>
+
+                        </form>
+                    </div>
+
+
+                </div>
 
             </div>
         )
